fe086528abb2b78870b5d3c19d2dcb7d0e2cca58
BaseAdam <adam.fiedko@gmail.com>
2026-06-04 13:13:25 +0200

    feat(core): wire up QueryClient and router providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { GlobalStyles } from './design-system/theme/GlobalStyles'
-import { theme } from './design-system/theme/theme'
+import { GlobalStyles } from './design-system'
+import { theme } from './design-system'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
